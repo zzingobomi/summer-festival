37fff1d834d7e8599a62ad96961dedd40392dac3
zzingo5 <zzingo5@gmail.com>
2023-09-07 16:58:08 +0900

    feat: lantern 인스턴스 생성

--- src/environment.ts
+++ src/environment.ts
@@ -1,17 +1,43 @@
 import {
+  Color3,
   Mesh,
   MeshBuilder,
+  PBRMetallicRoughnessMaterial,
   Scene,
   SceneLoader,
+  Texture,
   TransformNode,
   Vector3,
 } from "@babylonjs/core";
+import { Lantern } from "./lantern";
 
 export class Environment {
   private _scene: Scene;
 
+  // Meshes
+  private _lanternObjs: Array<Lantern>;
+  private _lightmtl: PBRMetallicRoughnessMaterial;
+
   constructor(scene: Scene) {
     this._scene = scene;
+    this._lanternObjs = [];
+
+    const lightmtl = new PBRMetallicRoughnessMaterial(
+      "lantern mesh light",
+      this._scene
+    );
+    lightmtl.emissiveTexture = new Texture(
+      "/textures/litLantern.png",
+      this._scene,
+      true,
+      false
+    );
+    lightmtl.emissiveColor = new Color3(
+      0.8784313725490196,
+      0.7568627450980392,
+      0.6235294117647059
+    );
+    this._lightmtl = lightmtl;
   }
 
   public async load() {
@@ -30,8 +56,19 @@ export class Environment {
       lanternInstance.isVisible = true;
       lanternInstance.setParent(lanternHolder);
 
-      //let newLantern = new Lantern(.....)
+      let newLantern = new Lantern(
+        this._lightmtl,
+        lanternInstance,
+        this._scene,
+        assets.env
+          .getChildTransformNodes(false)
+          .find((m) => m.name === "lantern " + i)
+          .getAbsolutePosition()
+      );
+      this._lanternObjs.push(newLantern);
     }
+
+    assets.lantern.dispose();
   }
 
   public async _loadAsset() {
